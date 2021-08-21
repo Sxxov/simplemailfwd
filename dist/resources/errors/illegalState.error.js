b1f0c54eda1172b8f5121ffe0827ba2cfa304c34
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-08-21 21:18:10 +0800

    🔨 @ 08-21-2021 21:18:10

--- dist/resources/errors/illegalState.error.js
+++
@@ -1,6 +0,0 @@
-import { ClientError } from './client.error.js';
-export class IllegalStateError extends ClientError {
-    constructor(message) {
-        super(`Illegal state${message ? `: ${message}` : ''}`);
-    }
-}
