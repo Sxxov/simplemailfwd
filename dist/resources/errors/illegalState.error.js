2efe849a2fe64d07a255f5751900440f5a48e2da
jason lim <50891910+Sxxov@users.noreply.github.com>
2021-08-21 20:52:25 +0800

    Dawn of all commits

---
+++ dist/resources/errors/illegalState.error.js
@@ -0,0 +1,6 @@
+import { ClientError } from './client.error.js';
+export class IllegalStateError extends ClientError {
+    constructor(message) {
+        super(`Illegal state${message ? `: ${message}` : ''}`);
+    }
+}
